2a5964f73eca1a557d0d0c8c71ef446bae94594c
Drake Setera <47800977+DrakeSeteraO@users.noreply.github.com>
2025-06-02 17:33:44 -0400

    Made while loops single lined

--- MergeSort.py
+++ MergeSort.py
@@ -10,20 +10,17 @@ def MergeSort(lst: list) -> list:
                 complete = True
             elif p2 >= len(lst):
                 complete = True
-                while p3 < len(lst):
-                        temp[p3] = lst[p1]; p1, p3 = p1 + 1, p3 + 1
+                while p3 < len(lst): temp[p3] = lst[p1]; p1, p3 = p1 + 1, p3 + 1
             else:
                 if lst[p1] <= lst[p2]:
                     temp[p3] = lst[p1]; p1, p3 = p1 + 1, p3 + 1
                     if p1 % size == 0:
-                        while p2 % (2 * size) != 0:
-                            temp[p3] = lst[p2]; p2, p3 = p2 + 1, p3 + 1 
+                        while p2 % (2 * size) != 0: temp[p3] = lst[p2]; p2, p3 = p2 + 1, p3 + 1 
                         p1, p2 = p1 + size, p2 + size
                 else:
                     temp[p3] = lst[p2]; p2, p3 = p2 + 1, p3 + 1
                     if p2 % size == 0:
-                        while p1 % (2 * size) != size:
-                            temp[p3] = lst[p1]; p1, p3 = p1 + 1, p3 + 1
+                        while p1 % (2 * size) != size: temp[p3] = lst[p1]; p1, p3 = p1 + 1, p3 + 1
                         p1, p2 = p1 + size, p2 + size
                         
         lst = temp.copy()
