31d9a6c3db4724f0f7d2637214d4829b299d10f3
Drake Setera <47800977+DrakeSeteraO@users.noreply.github.com>
2025-06-02 17:26:04 -0400

    Merge Sort without recursion

---
+++ MergeSort.py
@@ -0,0 +1,51 @@
+from math import log2, ceil
+
+def MergeSort(lst: list) -> list:
+    temp = [0] * len(lst)
+    for i in range(ceil(log2(len(lst)))):
+        size = 2 ** i
+        complete = False
+        
+        p1 = 0
+        p2 = size
+        p3 = 0
+        while not complete:
+            if p1 >= len(lst):
+                complete = True
+            elif p2 >= len(lst):
+                complete = True
+                while p3 < len(lst):
+                        temp[p3] = lst[p1]
+                        p1 += 1
+                        p3 += 1
+            else:
+                if lst[p1] <= lst[p2]:
+                    temp[p3] = lst[p1]
+                    p1 += 1
+                    p3 += 1
+                    if p1 % size == 0:
+                        while p2 % (2 * size) != 0:
+                            temp[p3] = lst[p2]
+                            p2 += 1
+                            p3 += 1 
+                        p1 += size
+                        p2 += size 
+        
+                else:
+                    temp[p3] = lst[p2]
+                    p2 += 1
+                    p3 += 1
+                    if p2 % size == 0:
+                        while p1 % (2 * size) != size:
+                            temp[p3] = lst[p1]
+                            p1 += 1
+                            p3 += 1
+                        p1 += size
+                        p2 += size
+                        
+        lst = temp.copy()
+    return lst
+
+
+lst = [4,2,3,1,0]
+print(MergeSort(lst))                  
