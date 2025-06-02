b92ba8706aac3bc888f2e63fad58b5d730af00be
Drake Setera <47800977+DrakeSeteraO@users.noreply.github.com>
2025-06-02 17:51:05 -0400

    Removed some while loops
    Replaced some while loops with list comprehension
    Created a lambda function

--- MergeSort.py
+++ MergeSort.py
@@ -1,31 +1,33 @@
 from math import log2, ceil
 
+
 def MergeSort(lst: list) -> list:
     temp = [0] * len(lst)
     for i in range(ceil(log2(len(lst)))):
-        size = 2 ** i; complete, p1, p2, p3 = False, 0, size, 0
+        increment = lambda x, y: [x + 1, y + 1]; size = 2 ** i; complete, p1, p2, p3 = False, 0, size, 0; cur = 0
 
         while not complete:
             if p1 >= len(lst):
                 complete = True
             elif p2 >= len(lst):
                 complete = True
-                while p3 < len(lst): temp[p3] = lst[p1]; p1, p3 = p1 + 1, p3 + 1
+                while p3 < len(lst): temp[p3] = lst[p1]; p1, p3 = increment(p1, p3)
             else:
                 if lst[p1] <= lst[p2]:
-                    temp[p3] = lst[p1]; p1, p3 = p1 + 1, p3 + 1
+                    temp[p3] = lst[p1]; p1, p3 = increment(p1, p3)
                     if p1 % size == 0:
-                        while p2 % (2 * size) != 0: temp[p3] = lst[p2]; p2, p3 = p2 + 1, p3 + 1 
-                        p1, p2 = p1 + size, p2 + size
+                        temp[p3:(cur+1) * size * 2] = lst[p2:2 * (cur) * size + (2 * size)]
+                        p1, p2, p3 = (cur+1) * size * 2, (cur+1) * size * 2 + size, (cur+1) * 2 * size
                 else:
-                    temp[p3] = lst[p2]; p2, p3 = p2 + 1, p3 + 1
+                    temp[p3] = lst[p2]; p2, p3 = increment(p2, p3)
                     if p2 % size == 0:
-                        while p1 % (2 * size) != size: temp[p3] = lst[p1]; p1, p3 = p1 + 1, p3 + 1
-                        p1, p2 = p1 + size, p2 + size
+                        temp[p3:(cur+1) * size * 2] = lst[p1:2 * (cur) * size + size]
+                        p1, p2, p3 = (cur+1) * size * 2, (cur+1) * size * 2 + size, (cur+1) * 2 * size
+            cur += 1
                         
         lst = temp.copy()
     return lst
 
 
-lst = [4,2,3,1,0, -1]
+lst = [4,2,7,1,0, -1, 5]
 print(MergeSort(lst))                  
