7551f5134c83f895eabf9641224684a9cc3d144d
Drake Setera <47800977+DrakeSeteraO@users.noreply.github.com>
2025-06-02 18:16:24 -0400

    Created new lambda function
    New lambda function to condense code
    Changed incrementation of cur

--- MergeSort.py
+++ MergeSort.py
@@ -4,23 +4,23 @@ from math import log2, ceil
 def MergeSort(lst: list) -> list:
     temp = [0] * len(lst)
     for i in range(ceil(log2(len(lst)))):
-        increment = lambda x, y: [x + 1, y + 1]; size = 2 ** i; complete, p1, p2, p3 = False, 0, size, 0; cur = 0
+        update = lambda cur, size: [(cur) * size * 2, size * ((cur) * 2 + 1), (cur) * 2 * size];size = 2 ** i; complete, p1, p2, p3 = False, 0, size, 0; cur = 1
 
         while not complete:
             if p1 >= len(lst):
                 complete = True
             elif p2 >= len(lst):
                 complete = True
-                while p3 < len(lst): temp[p3] = lst[p1]; p1, p3 = increment(p1, p3)
+                while p3 < len(lst): temp[p3] = lst[p1]; p1, p3 = p1 + 1, p3 + 1
             else:
                 if lst[p1] <= lst[p2]:
-                    temp[p3] = lst[p1]; p1, p3 = increment(p1, p3)
+                    temp[p3] = lst[p1]; p1, p3 = p1 + 1, p3 + 1
                     if p1 % size == 0:
-                        temp[p3:(cur+1) * size * 2] = lst[p2:2 * (cur + 1) * size]; p1, p2, p3 = (cur+1) * size * 2, size * ((cur+1) * 2 + 1), (cur+1) * 2 * size
+                        temp[p3:(cur) * size * 2] = lst[p2:2 * (cur) * size]; p1, p2, p3 = update(cur, size)
                 else:
-                    temp[p3] = lst[p2]; p2, p3 = increment(p2, p3)
+                    temp[p3] = lst[p2]; p2, p3 = p2 + 1, p3 + 1
                     if p2 % size == 0:
-                        temp[p3:(cur+1) * size * 2] = lst[p1:size * (2 * cur + 1)]; p1, p2, p3 = (cur+1) * size * 2, size * ((cur+1) * 2 + 1), (cur+1) * 2 * size
+                        temp[p3:(cur) * size * 2] = lst[p1:size * (2 * (cur-1) + 1)]; p1, p2, p3 = update(cur, size)
             cur += 1
                         
         lst = temp.copy()
