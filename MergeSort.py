07aea0fe02038033c82405ad8899729c3c631bec
Drake Setera <47800977+DrakeSeteraO@users.noreply.github.com>
2025-06-03 12:55:30 -0400

    Compressed 1 line if else statements
    Removed unnecessary brackets
    Added cur to the update lambda function to reduce the required characters
    Moved 1 line if else statements to the same line as if statements

--- MergeSort.py
+++ MergeSort.py
@@ -5,7 +5,7 @@ def MergeSort(lst: list) -> list:
     length = len(lst)
     temp = [0] * length
     for i in range(ceil(log2(length))):
-        update = lambda cur, size: [cur * size * 2, size * (cur * 2 + 1), cur * 2 * size];size = 2 ** i; complete, p1, p2, p3 = False, 0, size, 0; cur = 1
+        update = lambda cur, size: [cur * size * 2, size * (cur * 2 + 1), cur * 2 * size, cur + 1];size = 2 ** i; complete, p1, p2, p3 = False, 0, size, 0; cur = 1
 
         while not complete:
             if p1 >= length:
@@ -13,11 +13,8 @@ def MergeSort(lst: list) -> list:
             elif p2 >= length:
                 complete, temp[p3:] = True, lst[p1:((cur-1) * size * 2 + size) * (size * ((cur-1) * 2 + 1) < length) + size * (length * ((cur-1) * 2 + 1) >= length)]
             else:
-                if lst[p1] <= lst[p2]:
-                    temp[p3] = lst[p1]; p1, p3 = p1 + 1, p3 + 1; [[temp[p3:cur * size * 2]],[p1, p2, p3], cur] = [[lst[p2:2 * cur * size]], update(cur, size), cur + 1] if p1 % size == 0 else [[temp[p3:cur * size * 2]], [p1, p2, p3], cur]
-                    
-                else:
-                    temp[p3] = lst[p2]; p2, p3 = p2 + 1, p3 + 1; [[temp[p3:cur * size * 2]],[p1, p2, p3], cur] = [[lst[p1:size * (2 * (cur-1) + 1)]], update(cur, size), cur + 1] if p2 % size == 0 else [[temp[p3:cur * size * 2]], [p1, p2, p3], cur]
+                if lst[p1] <= lst[p2]: temp[p3], p1, p3 = lst[p1], p1 + 1, p3 + 1; [[temp[p3:cur * size * 2]],[p1, p2, p3, cur]] = [[lst[p2:2 * cur * size]], update(cur, size)] if p1 % size == 0 else [[temp[p3:cur * size * 2]], [p1, p2, p3, cur]]    
+                else: temp[p3], p2, p3 = lst[p2], p2 + 1, p3 + 1; [[temp[p3:cur * size * 2]],[p1, p2, p3, cur]] = [[lst[p1:size * (2 * (cur-1) + 1)]], update(cur, size)] if p2 % size == 0 else [[temp[p3:cur * size * 2]], [p1, p2, p3, cur]]
             
                         
         lst = temp.copy()
